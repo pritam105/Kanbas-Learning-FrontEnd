aaccbdb6fb3fbcd679feabeaf251300c7a59cd03
pritam105 <pritammane105@mail.com>
2024-09-19 17:23:08 -0400

    redirect home page to labs

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -139,7 +139,7 @@ export default function Lab1() {
         <br />
         Loading a local image:
         <br />
-        <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" />
+        <img id="wd-teslabot" src="images/teslabot.avif" height="200px" />
       </div>
 
       <div id="wd-forms">
