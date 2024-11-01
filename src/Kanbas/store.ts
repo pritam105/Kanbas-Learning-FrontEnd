85074c9caf41f6cf8779b562b25a15c5596e837c
pritam105 <pritammane105@mail.com>
2024-10-31 21:35:18 -0400

    add assignment reducer

--- src/Kanbas/store.ts
+++ src/Kanbas/store.ts
@@ -2,10 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import modulesReducer from "./Courses/Modules/reducer";
 import accountReducer from "./Account/reducer";
 import enrollmentReducer from "./reducer";
+import assignmentReducer from "./Courses/Assignments/reducer";
 
 const store = configureStore({
   reducer: {
-    modulesReducer, accountReducer, enrollmentReducer
+    modulesReducer, accountReducer, enrollmentReducer, assignmentReducer
   },
 });
 export default store;
